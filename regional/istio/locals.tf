f89321a8548cbfdd31f1e2b4ccb829c50e6ccdb5
Brett Curtis <brett@osinfra.io>
2024-08-03 08:57:28 -0400

    Bump `terraform-kubernetes-istio` Terraform child module version

--- regional/istio/locals.tf
+++ regional/istio/locals.tf
@@ -2,5 +2,14 @@
 # https://www.terraform.io/docs/language/values/locals.html
 
 locals {
+
+  labels = {
+    cost-center = "x001"
+    env         = var.environment
+    repository  = "google-cloud-kubernetes"
+    platform    = "google-cloud-kubernetes"
+    team        = "platform-google-cloud-kubernetes"
+  }
+
   regional = data.terraform_remote_state.regional.outputs
 }
