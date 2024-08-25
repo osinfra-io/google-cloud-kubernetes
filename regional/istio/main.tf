aa0cb9af999a405ffa36b63d38740a9253785370
Brett Curtis <brett@osinfra.io>
2024-08-25 11:52:39 -0400

    Comment out us-east4-a cluster

--- regional/istio/main.tf
+++ regional/istio/main.tf
@@ -106,9 +106,9 @@ module "kubernetes_istio" {
     {
       "link" = "us-east1/services-us-east1-b"
     },
-    {
-      "link" = "us-east4/services-us-east4-a"
-    }
+    # {
+    #   "link" = "us-east4/services-us-east4-a"
+    # }
   ]
 
   project = local.regional.project_id
