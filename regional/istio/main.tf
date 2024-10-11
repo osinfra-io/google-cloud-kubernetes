9bfe83aaf35def9c2beee05df3e5295ea3c6630d
Brett Curtis <brett@osinfra.io>
2024-10-11 16:21:32 -0400

    Refactor OPA Gatekeeper workflow dependencies and update Istio module source references

--- regional/istio/main.tf
+++ regional/istio/main.tf
@@ -82,7 +82,7 @@ data "terraform_remote_state" "regional" {
 # https://github.com/osinfra-io/terraform-kubernetes-istio
 
 module "kubernetes_istio" {
-  source = "github.com/osinfra-io/terraform-kubernetes-istio//regional?ref=main"
+  source = "github.com/osinfra-io/terraform-kubernetes-istio//regional?ref=cert"
 
   artifact_registry          = "us-docker.pkg.dev/plt-lz-services-tf79-prod/plt-docker-virtual"
   cluster_prefix             = "plt"
