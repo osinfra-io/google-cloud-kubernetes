6049315eacb8d3d75f18ec2fad5995e094ee2f0b
Brett Curtis <brett@osinfra.io>
2024-10-13 15:17:05 -0400

    Testing

--- regional/cert-manager/istio-csr/main.tf
+++ regional/cert-manager/istio-csr/main.tf
@@ -85,5 +85,5 @@ module "kubernetes_cert_manager_istio_csr" {
   source = "github.com/osinfra-io/terraform-kubernetes-cert-manager//regional/istio-csr?ref=main"
 
   artifact_registry = "us-docker.pkg.dev/plt-lz-services-tf79-prod/plt-docker-virtual"
-  cluster_id        = var.cert_manager_istio_csr_cluster_id
+  cluster_prefix    = "plt"
 }
