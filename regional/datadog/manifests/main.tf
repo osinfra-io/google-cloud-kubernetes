71aac3a467b2f7e187e5fe120fe857759ed0e963
Brett Curtis <brett@osinfra.io>
2024-11-15 07:33:40 -0500

    Enable Datadog integration and update module references in manifests

--- regional/datadog/manifests/main.tf
+++ regional/datadog/manifests/main.tf
@@ -1,12 +1,12 @@
 # Kubernetes Datadog Operator Module (osinfra.io)
 # https://github.com/osinfra-io/terraform-kubernetes-datadog-operator
 
-# module "kubernetes_datadog_operator_manifests" {
-#   source = "github.com/osinfra-io/terraform-kubernetes-datadog-operator//regional/manifests?ref=v0.1.3"
+module "kubernetes_datadog_operator_manifests" {
+  source = "github.com/osinfra-io/terraform-kubernetes-datadog-operator//regional/manifests?ref=disable"
 
-#   api_key                 = var.datadog_api_key
-#   app_key                 = var.datadog_app_key
-#   kubernetes_cluster_name = data.google_container_cluster.name
-#   registry                = "us-docker.pkg.dev/plt-lz-services-tf79-prod/plt-docker-virtual"
-#   team                    = "platform-google-cloud-kubernetes"
-# }
+  api_key        = var.datadog_api_key
+  app_key        = var.datadog_app_key
+  cluster_prefix = "plt"
+  registry       = "us-docker.pkg.dev/plt-lz-services-tf79-prod/plt-docker-virtual"
+  team           = "platform-google-cloud-kubernetes"
+}
